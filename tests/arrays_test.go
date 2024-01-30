1a5fe74c96ffd9a626b6e49842b3f95970b08309
zyad <zyadhimself1@gmail.com>
2024-01-31 01:20:24 +0200

    learned alot

--- tests/arrays_test.go
+++ tests/arrays_test.go
@@ -1,11 +1,28 @@
 package tests
-import "testing"
-import "github.com/Ozzy-ZY/proj/internal/arrays"
-func TestSum(t *testing.T){
-	numbers := [5]int{1,2,3,4,5}
-	sum := arrays.Sum(numbers[:])
-	exp := 15
-	if sum != exp {
-		t.Errorf("exp '%d' but found '%d' given'%v'",exp,sum,numbers)
-	}
+
+import (
+	"testing"
+
+	"github.com/Ozzy-ZY/proj/internal/arrays"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("colletion of 5 items", func(t *testing.T) {
+		nums := [5]int{1, 2, 3, 4, 5}
+		got := arrays.Sum(nums[0:1]) //the [:] is creating a silce out of 
+											 //the original array and : is a kind of a
+											 //range operator that is not inclusive
+		want := 1
+		if got != want {
+			t.Errorf("got %d want %d given %d", got, want, nums)
+		}
+	})
+	t.Run("colletion of any size", func(t *testing.T) {
+		nums := []int{2, 3, 5}
+		got := arrays.Sum(nums[:])
+		want := 10
+		if want != got {
+			t.Errorf("got %d want %d given %d", got, want, nums)
+		}
+	})
 }
