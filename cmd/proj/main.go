7133477b598205b374035e9b79f883b107ed0606
zyad <zyadhimself1@gmail.com>
2024-02-06 17:23:50 +0200

    followed the docs

--- cmd/proj/main.go
+++ cmd/proj/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Ozzy-ZY/proj/internal/helloworld"
 )
 
 func main() {
-	fmt.Println(helloworld.Helloworld("zyad"))
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+	message , err := helloworld.Hello("")
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
--- internal/helloworld/helloworld.go
+++ internal/helloworld/helloworld.go
@@ -1,7 +1,33 @@
 package helloworld
 
-var Ozzy = 19
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Helloworld(name string) string {
-	return "hello" + name
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return name, errors.New("empty name")
+	}
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
